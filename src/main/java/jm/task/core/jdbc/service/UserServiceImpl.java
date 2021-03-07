a0b5eef1969eaee5483dfeab32d62d9697359d31
Andrey Zubkov <zubkoff.id@gmail.com>
2021-03-07 22:38:15 +0300

    fixed the work of the database with the dao

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,97 +1,36 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private Util util = new Util();
-    List<User> userList;
+    private UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
 
     public void createUsersTable() {
-        util.getInstance();
-        Statement statement;
-        try {
-            statement = util.getConnection().createStatement();
-            String query = "CREATE TABLE IF NOT EXISTS my_db_jm.users (" +
-                    "id INT NOT NULL AUTO_INCREMENT," +
-                    "name VARCHAR (40)," +
-                    "lastName VARCHAR (40)," +
-                    "age INT," +
-                    "PRIMARY KEY (id))";
-            statement.executeUpdate(query);
-            statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
-        util.getInstance();
-        Statement statement;
-        try {
-            statement = util.getConnection().createStatement();
-            String query = "DROP TABLE IF EXISTS my_db_jm.users";
-            statement.executeUpdate(query);
-            statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userDaoJDBC.dropUsersTable();
     }
 
-
     public void saveUser(String name, String lastName, byte age) {
-        util.getInstance();
-        PreparedStatement preparedStatement;
-        userList = new ArrayList<>();
-        try {
-            preparedStatement = util.getConnection().prepareStatement("INSERT INTO my_db_jm.users (name, lastname, age) VALUES (?, ?, ?)");
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setInt(3, age);
-            System.out.println("User с именем – " + name + " добавлен в базу данных");
-            userList.add(new User(name, lastName, age));
-            preparedStatement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userDaoJDBC.saveUser(name, lastName, age);
     }
 
 
     public void removeUserById(long id) {
-        util.getInstance();
-        Statement statement;
-        try {
-            statement = util.getConnection().createStatement();
-            String query = "DELETE FROM my_db_jm.users WHERE id = " + id;
-            statement.executeUpdate(query);
-            statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        util.getInstance();
-        return userList;
+        return userDaoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        util.getInstance();
-        Statement statement;
-        try {
-            statement = util.getConnection().createStatement();
-            String query = "DELETE FROM my_db_jm.users";
-            statement.execute(query);
-            userList.clear();
-            statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userDaoJDBC.cleanUsersTable();
     }
 }
